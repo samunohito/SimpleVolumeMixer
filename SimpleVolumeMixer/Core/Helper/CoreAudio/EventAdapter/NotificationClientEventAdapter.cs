f2e4ef3348359b0d3ad9cdff4f5706b4d08dac57
osamu <46447427+sam-osamu@users.noreply.github.com>
2022-04-14 09:50:16 +0900

    #5 コメント補強 CoreAudioAPIのEnumとイベント周り

--- SimpleVolumeMixer/Core/Helper/CoreAudio/EventAdapter/NotificationClientEventAdapter.cs
+++ SimpleVolumeMixer/Core/Helper/CoreAudio/EventAdapter/NotificationClientEventAdapter.cs
@@ -5,18 +5,54 @@ using SimpleVolumeMixer.Core.Helper.Component;
 
 namespace SimpleVolumeMixer.Core.Helper.CoreAudio.EventAdapter;
 
+/// <summary>
+/// Notifications when an audio endpoint device is added or removed, when the state or properties of an endpoint device change,
+/// or when there is a change in the default role assigned to an endpoint device.
+/// Since we do not want to block CoreAudioAPI in the app-side processing,
+/// notifications from CoreAudioAPI are stored once in <see cref="QueueProcessor{TP,TR}"/> and are notified to the app side asynchronously and sequentially.
+/// </summary>
+/// <remarks>
+/// We use information from MSDN and CSCore functions, so please refer to their documentation as well.
+/// The document text for some functions is taken from MSDN.
+/// <see cref="MMNotificationClient"/>
+/// https://docs.microsoft.com/ja-jp/windows/win32/api/mmdeviceapi/nn-mmdeviceapi-immnotificationclient
+/// </remarks>
 public class NotificationClientEventAdapter : DisposableComponent
 {
+    /// <summary>
+    /// The OnDeviceAdded method indicates that a new audio endpoint device has been added.
+    /// </summary>
     public event EventHandler<DeviceNotificationEventArgs>? DeviceAdded;
+
+    /// <summary>
+    /// The OnDeviceRemoved method indicates that an audio endpoint device has been removed.
+    /// </summary>
     public event EventHandler<DeviceNotificationEventArgs>? DeviceRemoved;
+
+    /// <summary>
+    /// The OnPropertyValueChanged method indicates that the value of a property belonging to an audio endpoint device has changed.
+    /// </summary>
     public event EventHandler<DevicePropertyChangedEventArgs>? DevicePropertyChanged;
+
+    /// <summary>
+    /// The OnDeviceStateChanged method indicates that the state of an audio endpoint device has changed.
+    /// </summary>
     public event EventHandler<DeviceStateChangedEventArgs>? DeviceStateChanged;
+
+    /// <summary>
+    /// The OnDefaultDeviceChanged method notifies the client that the default audio endpoint device for a particular device role has changed.
+    /// </summary>
     public event EventHandler<DefaultDeviceChangedEventArgs>? DefaultDeviceChanged;
 
     private readonly MMNotificationClient _client;
     private readonly ILogger _logger;
     private readonly QueueProcessor<object?, object?> _processor;
 
+    /// <summary>
+    /// ctor
+    /// </summary>
+    /// <param name="client">Object to subscribe to events from CoreAudioAPI</param>
+    /// <param name="logger">Used to log event details from CoreAudioAPI</param>
     public NotificationClientEventAdapter(MMNotificationClient client, ILogger logger)
     {
         _client = client;
--- SimpleVolumeMixer/Core/Helper/CoreAudio/Types/DataFlowType.cs
+++ SimpleVolumeMixer/Core/Helper/CoreAudio/Types/DataFlowType.cs
@@ -2,10 +2,35 @@
 
 namespace SimpleVolumeMixer.Core.Helper.CoreAudio.Types;
 
+/// <summary>
+/// The EDataFlow enumeration defines constants that indicate the direction in which audio data flows between an audio endpoint device and an application.
+/// </summary>
+/// <remarks>
+/// We use information from MSDN and CSCore functions, so please refer to their documentation as well.
+/// The document text for some functions is taken from MSDN.
+/// <see cref="DataFlow"/>
+/// https://docs.microsoft.com/en-us/windows/win32/api/mmdeviceapi/ne-mmdeviceapi-edataflow
+/// </remarks>
 public enum DataFlowType
 {
+    /// <summary>
+    /// It is an unknown state that does not belong to any of the states.
+    /// This is a value provided for the convenience of the application; the Windows API will not be notified of this value.
+    /// </summary>
     Unknown = int.MinValue,
+
+    /// <summary>
+    /// Audio rendering stream. Audio data flows from the application to the audio endpoint device, which renders the stream.
+    /// </summary>
     Render = DataFlow.Render,
+
+    /// <summary>
+    /// Audio capture stream. Audio data flows from the audio endpoint device that captures the stream, to the application.
+    /// </summary>
     Capture = DataFlow.Capture,
+
+    /// <summary>
+    /// Audio rendering or capture stream. Audio data can flow either from the application to the audio endpoint device, or from the audio endpoint device to the application.
+    /// </summary>
     All = DataFlow.All
 }
